864ee80494c58ff326e732086f8b94ec7491398b
Mateusz Glowinski <mglowinski93@gmail.com>
2019-01-24 23:10:15 +0100

    Sending sms ver 0.1

--- sketch_jul27a/sketch_jul27a.ino
+++ sketch_jul27a/sketch_jul27a.ino
@@ -4,7 +4,8 @@
 *********/
 
 #include <SoftwareSerial.h>
- 
+#include <QueueList.h>
+#include <String.h>
 
 // Configure software serial port
 SoftwareSerial SIM900(8, 9); 
@@ -16,21 +17,33 @@ bool fireAlarmState = 1;
 bool lastFireAlarmState = 1;
 bool centralDamageState = 1;
 bool lastCentralDamageState = 1;
+const short unsigned int sendingSMSRetries = 10;
 
-String phoneNumbers[] = {"+48605108182", "+48502159172"};
+//String phoneNumbers[] = {"+48605108182", "+48502159172"};
+String phoneNumbers[] = {"+48605108182"};
+
+QueueList <String> queue;
+
+String readResponseSIM900(bool printResponseInLogs=true);
+void logEvent(String text);
+void sendSMS(String text) ;
+bool responseSuccessful();
 
 void setup() 
 {
+  Serial.println("Setting up SIM900 module");
   pinMode(enableGPRSpin, OUTPUT);
   pinMode(fireAlarmPin, INPUT_PULLUP);
   pinMode(centralDamagePin, INPUT_PULLUP);
   
   SIM900.begin(19200);
+  Serial.begin(9600);
   /*
     Enable GPRS module
   */
+  /*
   while(1)
-  {
+  { 
     String response = "";
     SIM900.println("AT");
     delay(100);
@@ -49,12 +62,29 @@ void setup()
     delay(1000);
     digitalWrite(enableGPRSpin, LOW);
     delay(5000);
-  }   
+  }
+  */
   /*
     Waiting for establishing a signal
   */
-  delay(20000);
+  //delay(20000);
+    logEvent("Setting SIM900 module into TEXT mode");
+    int initCounter = 0;
+    String SMSModeMessage = "SIM900 set into TEXT mode";
+    while(!responseSuccessful())
+    {
+        SIM900.println("AT+CMGF=1");
+        initCounter++;
+        if(initCounter > 100)
+        {
+          SMSModeMessage = "Failed to setup SIM900 into TEXT mode";
+          break;  
+        }
+        delay(1000);
+    }
+    logEvent(SMSModeMessage);
 }
+
 
 void loop() 
 { 
@@ -66,13 +96,14 @@ void loop()
   {
     if (fireAlarmState == 0)
     { 
-        sendSMS("Fire Alarm");
+        queue.push("Fire Alarm");
     }else
     {
-      sendSMS("Fire Alarm Off");  
+        queue.push("Fire Alarm Off");  
     }
     lastFireAlarmState = fireAlarmState;
   }
+
 
  /*
   Checking damage of central status 
@@ -82,35 +113,78 @@ void loop()
   {
     if (centralDamageState == 0)
     { 
-        sendSMS("Damage on Central");
+        queue.push("Damage on Central");
     }else
     {
-      sendSMS("Damage on Central Fixed");  
+        queue.push("Damage on Central Fixed");  
     }
     lastCentralDamageState = centralDamageState;
   }
 
+  if(!queue.isEmpty())
+  { 
+     sendSMS(queue.pop()); 
+  }
 }
+
 
 void sendSMS(String text) 
 {
   for( unsigned int a = 0; a < sizeof(phoneNumbers)/sizeof(phoneNumbers[0]); a++)
   {
-    // AT command to set SIM900 to SMS mode
-    SIM900.println("AT+CMGF=1"); 
-    delay(100);
-   
-    SIM900.println("AT + CMGS = \"" + phoneNumbers[a] + "\""); 
-    delay(100);
-  
-    SIM900.println(text); 
-    delay(100);
-
-    // End AT command with a ^Z, ASCII code 26
-    SIM900.println((char)26); 
-    delay(100);
-    SIM900.println();
-    // Give module time to send SMS
-    delay(5000); 
+    String message_log = "\n\n=====================\nSending " + text + " to " + phoneNumbers[a];
+    logEvent(message_log);
+    for(int i=0; i<sendingSMSRetries; i++)
+    {
+      SIM900.println("AT + CMGS = \"" + phoneNumbers[a] + "\""); 
+      delay(100);
+      SIM900.println(text); 
+      delay(100);
+      readResponseSIM900(false);
+      // End AT command with a ^Z, ASCII code 26
+      SIM900.println((char)26);
+      delay(5000);
+      if(responseSuccessful())
+      {
+        message_log = text + " was sent to " + phoneNumbers[a];
+        logEvent(message_log);
+        break;  
+      }
+      message_log = "Sending" + text + " to " + phoneNumbers[a] + " failed. It was " + i + "/" + sendingSMSRetries + "tries";
+      logEvent(message_log);
+    }
   }
+}
+
+
+void logEvent(String text)
+{
+  Serial.println(text);
+}
+
+
+String readResponseSIM900(bool printResponseInLogs=true)
+{
+    String response = "";
+    
+    while(SIM900.available())
+    {
+      response += (char) SIM900.read();
+      delay(1);
+    }
+    
+    if(printResponseInLogs)
+    {
+      String logMessage = "Response is: " + response;
+      logEvent(logMessage);
+    }
+      
+    return response;
+}
+
+
+bool responseSuccessful()
+{
+  String response = readResponseSIM900();
+  return response.indexOf("OK\r")>0;
 }
